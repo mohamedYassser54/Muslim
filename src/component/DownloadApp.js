ffcd313b36f17ee26d64d4598ad82468524c517d
mohamed yasser <TeamKingmariout@gmail.com>
2024-02-01 23:35:04 +0200

    update  All project7

--- src/component/DownloadApp.js
+++ src/component/DownloadApp.js
@@ -2,7 +2,7 @@ import React from 'react'
 import img4 from './image/4.png'
 import img5 from './image/7.png'
 import app from "./Muslim.apk"
-import app2 from "./.DS_Store"
+// import app2 from "./.DS_Store"
 import style from './css/download.module.css'
 function DownloadApp() {
   return (
@@ -16,7 +16,7 @@ function DownloadApp() {
         </div>
       </div>
       </a>
-      <a href={app2} download>
+      <a href="{app2}" download>
       <div className={style.image}>
         <img src={img5} alt="" />
         <div className={style.text}>
