8fe2c23833f37e2bc779bdd6813e660c43238b99
mohamed yasser <mohamedyasser12346262@gmail.com>
2024-02-11 12:32:36 +0200

    update  All project18

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react";
 import svgrPlugin from "vite-plugin-svgr";
-
+import {VitePwa} from 'vite-plugin-pwa'
 // https://vitejs.dev/config/
 export default defineConfig({
   // This changes the output dir from dist to build
@@ -20,6 +20,45 @@ export default defineConfig({
   ],
   assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg','**/*.apk'], // Add more file extensions as needed, excluding .apk
 });
+const manifestForPlugin = {
+  registerType:'prompt',
+  includeAssests:['favicon.ico', "apple-touc-icon.png", "masked-icon.svg"],
+  manifest:{
+    name:"Muslim",
+    short_name:"Muslim",
+    icons:[{
+      src: '/android-chrome-192x192.png',
+      sizes:'192x192',
+      type:'image/png',
+      purpose:'favicon'
+    },
+    {
+      src:'/android-chrome-512x512.png',
+      sizes:'512x512',
+      type:'image/png',
+      purpose:'favicon'
+    },
+    {
+      src: '/apple-touch-icon.png',
+      sizes:'180x180',
+      type:'image/png',
+      purpose:'apple touch icon',
+    },
+    {
+      src: '/maskable_icon.png',
+      sizes:'512x512',
+      type:'image/png',
+      purpose:'any maskable',
+    }
+  ],
+  theme_color:'#181818',
+  background_color:'#e0cc3b',
+  display:"standalone",
+  scope:'/',
+  start_url:"/",
+  orientation:'portrait'
+  },
+};
 
 export const rollupOutputOptions = () => ({
   output: {
@@ -27,4 +66,5 @@ export const rollupOutputOptions = () => ({
       apk: ['/src/component/Muslim.apk'],
     },
   },
+  plugins:[react(),VitePwa(manifestForPlugin)]
 });
