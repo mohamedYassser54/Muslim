6829771d1b8401c40379df9e4a161711446f20b1
mohamed yasser <mohamedyasser12346262@gmail.com>
2024-02-11 05:48:29 +0200

    update  All project16 download

--- src/component/DownloadApp.jsx
+++ src/component/DownloadApp.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import img4 from './image/4.png';
 import style from './css/download.module.css';
+import apk from '/src/component/Muslim.apk'; // Use the correct relative path
 
 function DownloadApp() {
-  // Replace 'YOUR_APK_FILE_URL' with the actual URL of your hosted APK file
-  const apkFileUrl = './Muslim.apk';
+  const apkFileUrl = apk;
 
   return (
     <div className={style.allDown}>
@@ -13,7 +13,7 @@ function DownloadApp() {
           <div className={style.image}>
             <img src={img4} alt="" />
             <div className={style.text}>
-              <h5>Download the App Now!</h5>
+              <h5>Download the App Now</h5>
             </div>
           </div>
         </a>
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import svgrPlugin from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // This changes the out put dir from dist to build
+  // This changes the output dir from dist to build
   // comment this out if that isn't relevant for your project
   build: {
     outDir: "build",
@@ -18,4 +18,13 @@ export default defineConfig({
       },
     }),
   ],
+  assetsInclude: ['**/*.png', '**/*.jpg', '**/*.jpeg','**/*.apk'], // Add more file extensions as needed, excluding .apk
+});
+
+export const rollupOutputOptions = () => ({
+  output: {
+    manualChunks: {
+      apk: ['/src/component/Muslim.apk'],
+    },
+  },
 });
