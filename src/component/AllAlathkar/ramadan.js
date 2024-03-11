160a3a989b4a29d8e01cc4e2a75571d6205a2caa
mohamed yasser <mohamedyasser12346262@gmail.com>
2024-03-11 04:30:25 +0200

    update  project34

--- src/component/AllAlathkar/ramadan.js
+++ src/component/AllAlathkar/ramadan.js
@@ -1,27 +1,26 @@
-import React from 'react'
-import { NavLink } from 'react-router-dom'
-import video from './video/1.mp4'
-import style from '../css/ramadan.module.css'
-import  '../css//btnram.css'
-function ramadan() {
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import style from '../css/ramadan.module.css';
+
+function Ramadan() {
   return (
     <div className={style.alldiv}>
       <video className={style.video} autoPlay muted loop>
-        <source src={video}/>
+        <source src={process.env.PUBLIC_URL + '/1.mp4'} type="video/mp4" />
       </video>
       
       <div className="btnram">
-      <NavLink to="/Days">
-      <button class="learn-more">
-  <span class="circle" aria-hidden="true">
-  <span class="icon arrow"></span>
-  </span>
-  <span class="button-text">المهام</span>
-</button>
-</NavLink>
+        <NavLink to="/Days">
+          <button className="learn-more">
+            <span className="circle" aria-hidden="true">
+              <span className="icon arrow"></span>
+            </span>
+            <span className="button-text">المهام</span>
+          </button>
+        </NavLink>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
 
-export default ramadan
+export default Ramadan;
