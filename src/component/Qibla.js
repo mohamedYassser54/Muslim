15f560c9208801bbce355ab40a46e60b54668990
mohamed yasser <mohamedyasser12346262@gmail.com>
2024-04-21 20:47:19 +0200

    update 4

--- src/component/Qibla.js
+++ src/component/Qibla.js
@@ -1,27 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import img from './image/18.png' ;
-// Import your arrow SVG here
+import img from './image/18.png';
 import arrow from './arrow.png';
-import { right } from '@popperjs/core';
 
 const QiblaCompass = () => {
   const [qiblaDirection, setQiblaDirection] = useState(0);
 
   useEffect(() => {
     // Fetch Qibla direction
-       axios.get('https://api.aladhan.com/v1/qibla/25.4106386/51.1846025')
+    axios.get('https://api.aladhan.com/v1/qibla/25.4106386/51.1846025')
       .then(response => {
         setQiblaDirection(response.data.data.direction);
       })
       .catch(error => {
         console.error('Error fetching Qibla direction:', error);
       });
+
+    // Add event listener for device orientation
+    window.addEventListener('deviceorientation', handleOrientationChange);
+
+    // Clean up event listener on unmount
+    return () => {
+      window.removeEventListener('deviceorientation', handleOrientationChange);
+    };
   }, []);
+
+  const handleOrientationChange = (event) => {
+    // Update qiblaDirection based on device orientation
+    if (event.alpha) {
+      setQiblaDirection(event.alpha);
+    }
+  };
 
   return (
     <div className='QiblaCompass'>
-      {/* <h1>Qibla Compass</h1> */}
+      <h1>hello</h1>
       <Compass qiblaDirection={qiblaDirection} />
     </div>
   );
@@ -34,9 +47,10 @@ const Compass = ({ qiblaDirection }) => {
     borderRadius: '50%',
     border: '2px solid black',
     position: 'relative',
-    backgroundImage:`url(${img})`,
-    backgroundPosition:"center",
-    backgroundSize:"cover",
+    backgroundImage: `url(${img})`,
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    transition: 'transform 0.5s ease',
   };
 
   const needleStyle = {
@@ -44,20 +58,19 @@ const Compass = ({ qiblaDirection }) => {
     left: '50%',
     top: '50%',
     transform: `translate(-50%, -50%) rotate(${qiblaDirection}deg)`,
-    width: '50px', 
-    height: '50px', 
-    backgroundImage: `url(${arrow})`, 
+    width: '50px',
+    height: '50px',
+    backgroundImage: `url(${arrow})`,
     backgroundSize: 'contain',
     backgroundRepeat: 'no-repeat',
     backgroundPosition: 'center',
+    transition: 'transform 0.5s ease',
   };
 
   return (
-    
     <div style={compassStyle}>
       <div style={needleStyle}></div>
     </div>
-    
   );
 };
 
